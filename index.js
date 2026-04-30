31ab45f16a9b85aecc68ef482653a12001c40229
anish2500 <tmganish2500@gmail.com>
2026-04-30 19:17:28 +0545

    user-login-registration-successful

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './src/config/db.js';
+import authRoutes from './src/routes/auth.routes.js';
 dotenv.config();
 
 
 const PORT = process.env.PORT || 4000; 
 const app = express();
 
+app.use(express.json());
+
 
 app.get('/', (req, res)=>{
     res.send("Hello World!");
 });
-
+app.use('/api/auth', authRoutes);
 connectDB();
 app.listen(PORT, ()=>{
     console.log(`Server running at ${PORT}` );
---
+++ src/validations/auth.validation.js
@@ -0,0 +1,12 @@
+import { z } from 'zod'; 
+
+export const registerSchema = z.object({
+    fullName: z.string().min(2, 'Full Name is required'), 
+    email: z.string().email('Invalid email address'), 
+    password: z.string().min(6, 'Password must be at least 6 characters'),
+});
+
+export const loginSchema = z.object({
+    email: z.string().email('Invalid email address'), 
+    password: z.string().min(1, 'Password is required')
+});
