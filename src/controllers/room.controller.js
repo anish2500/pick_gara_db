ef382afc43cece9213fb01f6f0efe3bf25f26055
anish2500 <tmganish2500@gmail.com>
2026-06-09 17:17:50 +0545

    join-with-pin-integrated

--- src/controllers/room.controller.js
+++ src/controllers/room.controller.js
@@ -35,3 +35,34 @@ export const createRoom = async (req, res) =>{
 };
 
 
+export const joinRoom = async (req, res) => {
+    try {
+        const {pin} = req.body; 
+
+        if(!pin){
+            return res.status(400).json({ message: 'PIN is required'});
+
+        }
+
+
+        const room = await roomRepository.findByPin(pin);
+
+
+        if (!room){
+            return res.status(404).json({
+                message: 'Room not found or no longer exists'
+            });
+        }
+
+        const updatedRoom = await roomRepository.addMember(room._id, req.userId); 
+
+        res.json({
+            message: 'Joined room successfully', 
+            room: updatedRoom, 
+        }); 
+    }catch (error){
+        res.status(500).json({ message: 'Server error', error: error.message});
+    }
+}
+
+
--- src/routes/room.routes.js
+++ src/routes/room.routes.js
@@ -1,12 +1,13 @@
 import express from 'express'; 
 
 import { protect} from '../middlewares/auth.middleware.js'; 
-import { createRoom } from '../controllers/room.controller.js';
+import { createRoom, joinRoom } from '../controllers/room.controller.js';
 
 const router = express.Router(); 
 
 
 
 router.post('/', protect, createRoom); 
+router.post('/join', protect, joinRoom);
 
 export default router; 
