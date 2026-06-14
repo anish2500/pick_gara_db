bc48ef9630c1c4966aa215da3f367617cb018d65
anish2500 <tmganish2500@gmail.com>
2026-06-14 09:20:40 +0545

    vote-feature-implemented

--- src/routes/room.routes.js
+++ src/routes/room.routes.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import { protect} from '../middlewares/auth.middleware.js'; 
 import { createRoom, joinRoom , getActiveRooms, getRoomById} from '../controllers/room.controller.js';
+import { castVote } from '../controllers/vote.controller.js';
 
 const router = express.Router(); 
 
@@ -10,6 +11,8 @@ const router = express.Router();
 router.post('/', protect, createRoom); 
 router.post('/join', protect, joinRoom);
 router.get('/active', protect, getActiveRooms);
+router.post('/:roomId/vote', protect, castVote); 
 router.get('/:id', protect, getRoomById); 
+
 
 export default router; 
