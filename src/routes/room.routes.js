9ed6ad210bfd78d4c1794849a22d343db393b75e
anish2500 <tmganish2500@gmail.com>
2026-06-16 10:22:23 +0545

    room-delte-get-active-room-implemented

--- src/routes/room.routes.js
+++ src/routes/room.routes.js
@@ -1,7 +1,7 @@
 import express from 'express'; 
 
 import { protect} from '../middlewares/auth.middleware.js'; 
-import { createRoom, joinRoom , getActiveRooms, getRoomById} from '../controllers/room.controller.js';
+import { createRoom, joinRoom , getActiveRooms, getRoomById, deleteRoom,} from '../controllers/room.controller.js';
 import { castVote } from '../controllers/vote.controller.js';
 
 const router = express.Router(); 
@@ -12,6 +12,7 @@ router.post('/', protect, createRoom);
 router.post('/join', protect, joinRoom);
 router.get('/active', protect, getActiveRooms);
 router.post('/:roomId/vote', protect, castVote); 
+router.delete('/:id', protect, deleteRoom); 
 router.get('/:id', protect, getRoomById); 
 
 
